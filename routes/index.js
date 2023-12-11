86a80ad44bab07eda8bf4d13427ead04169034ab
yuan6636 <aleniverson777@gmail.com>
2023-12-11 14:57:39 +0800

    add basic structure for user restaurants & change index page style

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,29 @@ const express = require('express')
 const router = express.Router()
 
 const restaurants = require('./restaurants')
+const users = require('./users')
 
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 
 router.get('/', (req, res) => {
       res.redirect('/restaurants')
+})
+
+router.get('/register', (req, res) => {
+      return res.render('register')
+})
+
+router.get('/login', (req, res) => {
+      return res.render('login')
+})
+
+router.post('/login', (req, res) => {
+      return res.send(req.body)
+})
+
+router.post('/logout', (req, res) => {
+      return res.send('logout')
 })
 
 module.exports = router
