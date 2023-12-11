145510f8975165e77772d9a9de3813d4f4711ecc
yuan6636 <aleniverson777@gmail.com>
2023-12-11 15:39:46 +0800

    feature: implement register & change migration table name Restaurants to lowercase & add register and login message

--- migrations/20231108041221-create-restaurant.js
+++ migrations/20231108041221-create-restaurant.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Restaurants", {
+    await queryInterface.createTable('restaurants', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -56,6 +56,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Restaurants');
+    await queryInterface.dropTable('restaurants');
   }
 };
